93dda6f252a59d706f78dd8e46c8e0fa397e7010
Giovanni <gcapuano@schema31.it>
2026-01-30 10:04:22 +0100

    separo git hub mio da git lab azienda

--- esercizi JAVA e SPRING/00 programmi COMPLESSI/spring es complessi/programma_salvatore_hr/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.User; // Importa la classe Entity
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Puoi aggiungere metodi personalizzati, ma per ora va bene così
-}
