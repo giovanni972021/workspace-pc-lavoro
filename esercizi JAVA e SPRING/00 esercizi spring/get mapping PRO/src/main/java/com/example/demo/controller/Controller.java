03b82639be42696cc2776e95b04f4a683f12db2d
Giovanni <gcapuano@schema31.it>
2026-02-11 15:04:18 +0100

    add folder get mapping pro

--- esercizi JAVA e SPRING/00 esercizi spring/get mapping PRO/src/main/java/com/example/demo/controller/Controller.java
+++ esercizi JAVA e SPRING/00 esercizi spring/get mapping PRO/src/main/java/com/example/demo/controller/Controller.java
@@ -1,17 +1,27 @@
 package com.example.demo.controller;
 
-//librerie x annotazioni
-/*importo libreria per poter usare annotazione get mapping */
+// Import necessari per le annotazioni di Spring
 import org.springframework.web.bind.annotation.GetMapping;
-
-/*importo libreria per poter usare annotazione rest controller*/
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api") // Definisce la rotta base per tutti i metodi in questa classe
 public class Controller {
 
+    /**
+     * Risponde all'indirizzo: http://localhost:8080/api/ciao
+     */
     @GetMapping("/ciao")
     public String saluto() {
         return "Ciao dal server Spring!";
+    }
+
+    /**
+     * Risponde all'indirizzo: http://localhost:8080/api/addio
+     */
+    @GetMapping("/addio")
+    public String addio() {
+        return "Arrivederci dal server Spring!";
     }
 }
